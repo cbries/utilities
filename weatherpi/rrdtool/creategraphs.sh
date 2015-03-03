bff6dcb139aee89add2cab6d4a6f830a068ebebd
Christian Benjamin Ries <mail@christianbenjaminries.de>
2015-03-03 18:32:55 +0100

    changed graph format

--- weatherpi/rrdtool/creategraphs.sh
+++ weatherpi/rrdtool/creategraphs.sh
@@ -7,20 +7,23 @@ rrdtool graph graph00.png \
     --color=BACK#FFFFFF \
     --start end-1d --title "24 Hours" \
 	--x-grid MINUTE:10:HOUR:2:HOUR:2:0:%X \
-    DEF:temperature=weatherdata.rrd:temperature:AVERAGE \
+    --alt-autoscale --alt-y-grid --rigid \
+	DEF:temperature=weatherdata.rrd:temperature:AVERAGE \
 	DEF:humidity=weatherdata.rrd:humidity:AVERAGE \
-	DEF:altitude=weatherdata.rrd:pressure:AVERAGE \
-	DEF:pressure=weatherdata.rrd:pressure:AVERAGE \
-    LINE2:temperature#3366CC:"Temperature [°C]" \
-	LINE3:humidity#0000ff:"Humidity [%]" \
-	LINE4:altitude#ff0000:"Altitude [m]" \
-	LINE5:pressure#ff00ff:"Pressure [hPa]"
+	LINE3:temperature#3366CC:"Temperature [°C]" \
+	LINE3:humidity#ff00AA:"Humidity [%]" 
+	
+	#LINE2:altitude2#ff0000:"Altitude [m]" 
+	#LINE:pressure#ff00ff:"Pressure [hPa]"
+    #DEF:altitude=weatherdata.rrd:pressure:AVERAGE \
+    #DEF:pressure=weatherdata.rrd:pressure:AVERAGE \
 
 rrdtool graph graph01.png \
 	--height=150 --width=425 \
 	--color=BACK#FFFFFF \
 	--start end-1d --vertical-label "°C"  --title "Temperature of the last 24 hours" \
 	--x-grid MINUTE:30:HOUR:5:HOUR:5:0:%X \
+	--alt-autoscale --alt-y-grid --rigid \
 	DEF:temperature=weatherdata.rrd:temperature:AVERAGE \
 	LINE2:temperature#3366CC
 
@@ -29,6 +32,7 @@ rrdtool graph graph02.png \
     --color=BACK#FFFFFF \
     --start end-1d --vertical-label "%"  --title "Humidity of the last 24 hours" \
 	--x-grid MINUTE:30:HOUR:5:HOUR:5:0:%X \
+	--alt-autoscale --alt-y-grid --rigid \
     DEF:humidity=weatherdata.rrd:humidity:AVERAGE \
     LINE2:humidity#3366CC
 
@@ -37,6 +41,7 @@ rrdtool graph graph03.png \
     --color=BACK#FFFFFF \
     --start end-1d --vertical-label "m"  --title "Altitude of the last 24 hours" \
 	--x-grid MINUTE:30:HOUR:5:HOUR:5:0:%X \
+	--alt-autoscale --alt-y-grid --rigid \
     DEF:altitude=weatherdata.rrd:altitude:AVERAGE \
     LINE2:altitude#3366CC
 
@@ -45,6 +50,7 @@ rrdtool graph graph04.png \
     --color=BACK#FFFFFF \
     --start end-1d --vertical-label "hPa"  --title "Pressure of the last 24 hours" \
 	--x-grid MINUTE:30:HOUR:5:HOUR:5:0:%X \
+	--alt-autoscale --alt-y-grid --rigid \
     DEF:pressure=weatherdata.rrd:pressure:AVERAGE \
     LINE2:pressure#3366CC
 
